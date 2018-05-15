c7a6bb823330220731170ff261033afbe4d8a034
Wordless Echo <me@gongjianwei.com>
2018-05-15 12:15:19 +0800

    Add MD5, MD5 generator and fix typo

---
+++ fonts/md5-generator.sh
@@ -0,0 +1,22 @@
+FONTPATH=`pwd`/fonts
+FONT=NotoSansCJK.ttc
+
+
+if [ -f "$FONTPATH/$FONT" ]; then
+	MD5=`md5sum "$FONTPATH/$FONT" | cut -d ' ' -f1`
+	if [ -f "$FONTPATH/$FONT.md5" ]; then
+		OLDMD5=`cat "$FONTPATH/$FONT.md5"`
+		if [ "$MD5" = "$OLDMD5" ]; then
+			echo "No change of $FONT"
+			exit 0
+		fi
+	fi
+
+	echo "$MD5" > "$FONTPATH/$FONT.md5"
+	echo >> "$FONTPATH/$FONT.md5"
+	rm "$FONTPATH/$FONT.part"*
+	split -b 100M "$FONTPATH/$FONT" -d -a 1 "$FONTPATH/$FONT.part" >&1
+
+else
+	echo "No $FONT, skipping"
+fi
